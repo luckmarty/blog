5a7bf304a1241b0cbfa3774bf0f849ce91f59b53
luckmarty <luckmarty@gmail.com>
2022-12-25 00:06:07 -0300

    Commit Inicial

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+# Create your models here.
+class Post(models.Model):
+    autor = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    subtitulo = models.CharField(max_length=255)
+    conteudo = RichTextUploadingField()
+    data_publicacao = models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.titulo
---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import Post
+
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
